9b8f0a55fa94f148531fff07ae9daf27dafb7a18
YW <80232265@qq.com>
2020-11-20 16:16:57 +0800

    feat：新增多模块导入，单文件导出

---
+++ src/feedback/index.ts
@@ -0,0 +1,10 @@
+
+export {default as ActionSheet} from "./action-sheet"
+
+export {default as Drawer} from "./drawer"
+
+export {default as HalfScreenDialog} from "./half-screen-dialog"
+
+export {default as Loading} from "./loading"
+
+export {default as Toast} from "./toast"
---
+++ src/form/index.ts
@@ -0,0 +1,6 @@
+
+export {default as Checkbox} from "./checkbox"
+
+export {default as Radio} from "./radio"
+
+export {default as Switch} from "./switch"
